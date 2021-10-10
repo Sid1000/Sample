fb0fe8af851ddfccc96faa449e27ebd052c20358
Siddhant <siddhantpatil328@gmail.com>
2021-10-10 16:04:53 +0530

    first commit

---
+++ src/pages/Reports.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Reports() {
+    return (
+        <div className="report">
+            <h1>Support</h1>
+        </div>
+    )
+}
+
+export default Reports;
